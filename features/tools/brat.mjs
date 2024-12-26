00103d8b52bc055f5431acd715f25f0b08bb7da6
bálxzzy <balzz9a@gmail.com>
2024-12-27 01:25:32 +0800

    Add Brat Cmd

--- features/tools/brat.mjs
+++ features/tools/brat.mjs
@@ -1,1 +1,9 @@
-
+const handle = {
+  miyxious: ["brat", "sbrat"],
+  category: "#tools",
+  describe: "Create Brat Sticker",
+  run: async (m, { q, repl, conn }) => {
+    const text = m.args.length >= 1 ? m.args.join(" ") : (m.quoted && m.quoted.text) || ""
+    await conn.sendMessage(m.chat, { sticker: { url: `https://apii.ambalzz.biz.id/api/sticker/brat?text=${encodeURIComponent(text)}`}}, { quoted: m })
+  }
+export default handle
