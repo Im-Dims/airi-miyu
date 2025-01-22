fabbf9acfda247192f1df21f8168a8839c9c4c36
bálxzzy <balzz9a@gmail.com>
2025-01-22 17:08:28 +0800

    Update brat--gif.mjs

--- features/tools/brat--gif.mjs
+++ features/tools/brat--gif.mjs
@@ -6,4 +6,5 @@ const handle = {
     const text = m.args.length >= 1 ? m.args.join(" ") : (m.quoted && m.quoted.text) || ""
     await conn.sendMessage(m.chat, { sticker: { url: `https://apii.ambalzz.biz.id/api/sticker/bratV2?text=${encodeURIComponent(text)}`}}, { quoted: m })
   }
+}
 export default handle
